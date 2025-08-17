617ddb9413105a81286c00f764271bc1a0ebf436
Alen Thomas <133691975+AIstar007@users.noreply.github.com>
2025-08-17 13:55:21 +0530

    Update serializers.py

--- backend/main/serializers.py
+++ backend/main/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
-from .models import Task, Log
+from .models import Task, Log, AppRequest
+
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = "__all__"
 
+
 class LogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Log
+        fields = "__all__"
+
+
+class AppRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AppRequest
         fields = "__all__"
